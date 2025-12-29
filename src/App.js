f2224875422a68a9d7b9471ecb2cc2625532c4ce
jegandunghee <jaejea1217@gmail.com>
2025-12-29 15:54:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Routes, Route, HashRouter } from "react-router-dom"
+import Layout from "./layout/Layout"
+import MainPage from "./pages/MainPage"
+import CategoryPage from "./pages/CategoryPage"
+import GuidePage from "./pages/GuidePage"
+import DetailPage from "./pages/DetailPage" 
+import CartPage from "./pages/CartPage"
+import './assets/scss/global.scss'
+import ScrollToTop from "./ScrollToTop";
+
+const App = () => {
+  return (     
+      //BrowserRouter -> HashRouter로 변경   
+    <HashRouter>
+    <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route 
+            path="/" 
+            element={<MainPage />}
+          />
+          <Route
+            path="/guide"
+            element={<GuidePage />}
+          />
+          <Route 
+            path="/category/:category" 
+            element={<CategoryPage />} 
+          />
+          <Route
+            path="/product/:productId"
+            element={<DetailPage />}
+          />
+          <Route 
+            path="/cart" 
+            element={<CartPage />}
+          />
+          </Route>    
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
